d075117f9ee51813bd5e2a90ff78d60726226383
Gregory Maj <gregory.j.maj@gmail.com>
2022-01-02 11:16:52 -0600

    rm duplicates

--- test.py
+++
@@ -1,5 +0,0 @@
-import requests, json, pprint
-
-card_info = requests.get(f"https://api.scryfall.com/cards/named?fuzzy=Huntmaster of the Fells // Ravager of the Fells", auth=('user', 'pass')).json()
-
-pprint.pprint(card_info["card_faces"][0])
