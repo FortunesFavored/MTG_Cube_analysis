5aa630cdb91c95158e92357025ba9462989f4498
Gregory Maj <gregory.j.maj@gmail.com>
2022-01-03 09:59:26 -0600

    update

--- Cube_compiler/test.py
+++ Cube_compiler/test.py
@@ -3,3 +3,6 @@ import requests, json, pprint
 card_info = requests.get(f"https://api.scryfall.com/cards/named?fuzzy=Huntmaster of the Fells // Ravager of the Fells", auth=('user', 'pass')).json()
 
 pprint.pprint(card_info["card_faces"][0])
+
+with open("cube_compiled", "w") as outfile:
+    json.dump(card_info, outfile)
